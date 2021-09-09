376be7832ec78069e88e075c9b84e44fe1fd3430
sean <pulpfictionost@gmail.com>
2021-09-09 16:56:35 +0100

    Initial

---
+++ cpp/README.md
@@ -0,0 +1,5 @@
+## Compile
+
+```
+mkdir -p ./bin && g++ -std=c++0x main_1.cpp -o ./bin/main_1.bin
+```
---
+++ cpp/src/1_intro/compilers/main_1.cpp
@@ -0,0 +1,15 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Summing App !" << endl;
+    int a, b, sum;
+        
+    cin >> a;
+    cin >> b;
+                
+    sum = a + b;
+    cout << sum << endl;
+}
