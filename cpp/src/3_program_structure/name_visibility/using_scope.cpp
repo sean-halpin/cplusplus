e7f70b37dbc51d7cad99e6da208a03cdabd9069a
sean <pulpfictionost@gmail.com>
2021-09-10 14:09:36 +0100

    Name Visibility

--- cpp/src/3_program_structure/name_visibility/using_scope.cpp
+++ cpp/src/3_program_structure/name_visibility/using_scope.cpp
@@ -1,5 +1,5 @@
-// using and using namespace have validity only in the same block in which they are stated or in the entire source 
-// code file if they are used directly in the global scope. 
+// using and using namespace have validity only in the same block in which they are stated or in the entire source
+// code file if they are used directly in the global scope.
 
 // using namespace example
 #include <iostream>
@@ -7,22 +7,23 @@ using namespace std;
 
 namespace first
 {
-  int x = 5;
+    int x = 5;
 }
 
 namespace second
 {
-  double x = 3.1416;
+    double x = 3.1416;
 }
 
-int main () {
-  {
-    using namespace first;
-    cout << x << '\n';
-  }
-  {
-    using namespace second;
-    cout << x << '\n';
-  }
-  return 0;
+int main()
+{
+    {
+        using namespace first;
+        cout << x << '\n';
+    }
+    {
+        using namespace second;
+        cout << x << '\n';
+    }
+    return 0;
 }
